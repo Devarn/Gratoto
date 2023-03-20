4748727aee55dfd886e3ba3015454d6e2e49acb9
KaushalaSaranath <kaushala.saranath7@gmail.com>
2023-03-20 21:53:43 +0530

    result pages
    Results Page

---
+++ FlutterAppMain/lib/pages/result_page/result_page_model.dart
@@ -0,0 +1,16 @@
+import '../../config/ui_model.dart';
+import '/pages/who_we_are/who_we_are_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class ResultPageModel extends FlutterFlowModel {
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {}
+
+  /// Additional helper methods are added here.
+
+}
