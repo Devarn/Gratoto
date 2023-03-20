6b3e024c3ead7d81f996f3a9b089ac2901fdb81a
SShehan <sachinshehan20@gmail.com>
2023-03-20 05:36:01 +0530

    gcp update

--- gcp/main.py
+++ gcp/main.py
@@ -5,60 +5,48 @@ import numpy as np
 
 model = None
 interpreter = None
-input_index = None
-output_index = None
+input_idx = None
+output_idx = None
 
-BUCKET_NAME = "gratato-tf-model"
+BUCKET_NAME = "gratato-model"
 
-class_names = ['Potato___Early_blight',
-               'Potato___Late_blight',
-               'Potato___healthy',
-               'Tomato_Bacterial_spot',
-               'Tomato_Early_blight',
-               'Tomato_Late_blight',
-               'Tomato_Leaf_Mold',
-               'Tomato_Septoria_leaf_spot',
-               'Tomato_Spider_mites_Two_spotted_spider_mite',
-               'Tomato__Target_Spot',
-               'Tomato__Tomato_YellowLeaf__Curl_Virus',
-               'Tomato__Tomato_mosaic_virus',
-               'Tomato_healthy']
+diseases_names = ['Grape__Black_rot', 'Grape_Esca(Black_Measles)', 'Grape__Leaf_blight(Isariopsis_Leaf_Spot)',
+                  'Grape__healthy', 'Potato_Early_blight', 'Potato_Late_blight', 'Potato__healthy',
+                  'Tomato__Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato__Leaf_Mold',
+                  'Tomato__Septoria_leaf_spot', 'Tomato_Spider_mites Two-spotted_spider_mite', 'Tomato__Target_Spot',
+                  'Tomato__Tomato_Yellow_Leaf_Curl_Virus', 'Tomato_Tomato_mosaic_virus', 'Tomato__healthy']
 
 
-def download_blob(bucket_name, source_blob_name, destination_file_name):
+def download_blob(bucket, source_blob, download_location):
     storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
-    blob = bucket.blob(source_blob_name)
+    bucket = storage_client.get_bucket(bucket)
+    blob = bucket.blob(source_blob)
 
-    blob.download_to_filename(destination_file_name)
+    blob.download_to_filename(download_location)
 
-    print(f"Blob {source_blob_name} downloaded to {destination_file_name}.")
+    print(f"Blob {source_blob} downloaded to {download_location}.")
 
 
 def predict(request):
     global model
     if model is None:
-        download_blob(
-            BUCKET_NAME,
-            "models/my_model3.h5",
-            "/tmp/my_model3.h5",
-        )
+        download_blob(BUCKET_NAME, "model/my_model3.h5", "/tmp/my_model3.h5", )
         model = tf.keras.models.load_model("/tmp/my_model3.h5")
 
     image = request.files["file"]
 
     image = np.array(
-        Image.open(image).convert("RGB").resize((256, 256))  # image resizing
+        Image.open(image).convert("RGB").resize((256, 256))
     )
 
-    image = image / 255  # normalize the image in 0 to 1 range
+    image = image / 255
 
-    img_array = tf.expand_dims(image, 0)
+    img_array = np.expand_dims(image, 0)
     predictions = model.predict(img_array)
 
-    print("Predictions:", predictions)
+    print("Prediction :- ", predictions)
 
-    predicted_class = class_names[np.argmax(predictions[0])]
+    predicted_class = diseases_names[np.argmax(predictions[0])]
     confidence = round(100 * (np.max(predictions[0])), 2)
 
     return {"class": predicted_class, "confidence": confidence}
